fc41711e27416c04a09ae78417387ee7ff13903f
gekim0519 <gekim1993@gmail.com>
2019-06-28 13:39:08 -0400

    remove credentials

--- __pycache__/twitter_credentials.py
+++
@@ -1,9 +0,0 @@
-# Variables that contains the user credentials to access Twitter API 
-consumer_key = "dkPY4LbxqaLSmbeGR3pdVG1by"
-consumer_secret = "YMSNEFT4HhZb8wjkiU1BhddnLomG2QUoPgkQCji6sTIvWWdRvb"
-access_token = "942800078011551744-MlvmDWmVwCLbJdOXxcDHcEMXCITMpDz"
-access_token_secret = "m0WWxOF1WjqAWqYtSIJ5ZIY4Acy5IYBmFvKj82EyOim7d"
-
-# aws stuff
-aws_access_key = 'AKIAJZ24AGOVMCYTJA2A'
-aws_access_secret_key = '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'
